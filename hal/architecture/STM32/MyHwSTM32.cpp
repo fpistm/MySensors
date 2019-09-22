a6eed2562c32032864dee487f064fc6eb2c2b4a5
Frederic Pillon <frederic.pillon@st.com>
2019-09-22 19:26:46 +0200

    Added STM32 core support
    See https://github.com/stm32duino/Arduino_Core_STM32
    
    On going.
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>
    
    # Conflicts:
    #	MySensors.h
    #	library.json
    #	library.properties

---
+++ hal/architecture/STM32/MyHwSTM32.cpp
@@ -0,0 +1,127 @@
+/*
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2019 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/MySensors/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ */
+
+#include "MyHwSTM32.h"
+
+bool hwInit(void)
+{
+#if !defined(MY_DISABLED_SERIAL)
+	MY_SERIALDEVICE.begin(MY_BAUD_RATE);
+#if defined(MY_GATEWAY_SERIAL)
+	while (!MY_SERIALDEVICE) {}
+#endif
+#endif
+	for (int i = 0 ; i < EEPROM.length() ; i++) {
+		EEPROM.write(i, 0);
+	}
+	return true;
+}
+
+void hwReadConfigBlock(void *buf, void *addr, size_t length)
+{
+	uint8_t *dst = static_cast<uint8_t *>(buf);
+	int pos = reinterpret_cast<int>(addr);
+	while (length-- > 0) {
+		*dst++ = EEPROM.read(pos++);
+	}
+}
+
+void hwWriteConfigBlock(void *buf, void *addr, size_t length)
+{
+	uint8_t *src = static_cast<uint8_t *>(buf);
+	int pos = reinterpret_cast<int>(addr);
+	while (length-- > 0) {
+		EEPROM.write(pos++, *src++);
+	}
+}
+
+uint8_t hwReadConfig(const int addr)
+{
+	uint8_t value;
+	hwReadConfigBlock(&value, reinterpret_cast<void *>(addr), 1);
+	return value;
+}
+
+void hwWriteConfig(const int addr, uint8_t value)
+{
+	hwWriteConfigBlock(&value, reinterpret_cast<void *>(addr), 1);
+}
+
+int8_t hwSleep(uint32_t ms)
+{
+	// TODO: Not supported!
+	(void)ms;
+	return MY_SLEEP_NOT_POSSIBLE;
+}
+
+int8_t hwSleep(const uint8_t interrupt, const uint8_t mode, uint32_t ms)
+{
+	// TODO: Not supported!
+	(void)interrupt;
+	(void)mode;
+	(void)ms;
+	return MY_SLEEP_NOT_POSSIBLE;
+}
+
+int8_t hwSleep(const uint8_t interrupt1, const uint8_t mode1, const uint8_t interrupt2,
+               const uint8_t mode2,
+               uint32_t ms)
+{
+	// TODO: Not supported!
+	(void)interrupt1;
+	(void)mode1;
+	(void)interrupt2;
+	(void)mode2;
+	(void)ms;
+	return MY_SLEEP_NOT_POSSIBLE;
+}
+
+
+void hwRandomNumberInit(void)
+{
+	//TODO: Add RNG API to the core
+}
+
+bool hwUniqueID(unique_id_t *uniqueID)
+{
+	//TODO: Add getUID API in the core using LL_GetUID_Wordx
+	(void)uniqueID;
+	return true;
+}
+
+uint16_t hwCPUVoltage(void)
+{
+	return 0;
+}
+
+uint16_t hwCPUFrequency(void)
+{
+	return F_CPU/100000UL;
+}
+
+int8_t hwCPUTemperature(void)
+{
+	return 0;
+}
+
+uint16_t hwFreeMem()
+{
+	//Not yet implemented
+	return FUNCTION_NOT_SUPPORTED;
+}
